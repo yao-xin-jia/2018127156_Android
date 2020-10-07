e1293e50492a488d3ac95f6da579171b40ad2f70
yxj-yxj <894548575@qq.com>
2020-10-07 19:09:42 +0800

    singleTop模式

--- FourPattern-HW2/app/src/main/java/com/yxj/fourpattern/FirstActivity.java
+++ FourPattern-HW2/app/src/main/java/com/yxj/fourpattern/FirstActivity.java
@@ -9,17 +9,18 @@ import android.view.View;
 import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
+    private static final String TAG = "FirstActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("FirstActivity", this.toString());
+        Log.d(TAG, this.toString());
         setContentView(R.layout.first_layout);
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(FirstActivity.this, FirstActivity.class));
+                startActivity(new Intent(FirstActivity.this, SecondActivity.class));
 
             }
         });
--- FourPattern-HW2/app/src/main/java/com/yxj/fourpattern/SecondActivity.java
+++ FourPattern-HW2/app/src/main/java/com/yxj/fourpattern/SecondActivity.java
@@ -2,13 +2,26 @@ package com.yxj.fourpattern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, this.toString());
         setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SecondActivity.this,FirstActivity.class));
+            }
+        });
     }
 }
