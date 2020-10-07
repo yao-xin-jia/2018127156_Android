032c4bf5cffc1f5fdcd8a3cdfd4ce0998568e1c3
yxj-yxj <894548575@qq.com>
2020-10-07 18:50:55 +0800

    建立三个活动

---
+++ FourPattern-HW2/app/src/main/java/com/yxj/fourpattern/Thirdctivity.java
@@ -0,0 +1,14 @@
+package com.yxj.fourpattern;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Thirdctivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+    }
+}
