08473bfd5af8071d163f0c78cb4838991d21e161
yxj-yxj <894548575@qq.com>
2020-10-07 18:57:40 +0800

    standard模式

--- FourPattern-HW2/app/src/main/java/com/yxj/fourpattern/FirstActivity.java
+++ FourPattern-HW2/app/src/main/java/com/yxj/fourpattern/FirstActivity.java
@@ -2,13 +2,27 @@ package com.yxj.fourpattern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("FirstActivity", this.toString());
         setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstActivity.this, FirstActivity.class));
+
+            }
+        });
+
     }
 }
