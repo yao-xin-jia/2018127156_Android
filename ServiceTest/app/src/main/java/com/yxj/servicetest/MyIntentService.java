d9e7cccf08bef34e26a622e8a050e1e8ed00de80
yxj <894548575@qq.com>
2020-11-22 12:18:34 +0800

    子线程的程序框架，消息处理机制，多线程之间的数据交换。异步任务的隐含子线程程序框架，服务，子服务的绑定

---
+++ ServiceTest/app/src/main/java/com/yxj/servicetest/MyIntentService.java
@@ -0,0 +1,26 @@
+package com.yxj.servicetest;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MyIntentService extends IntentService {
+    private static final String TAG = "MyIntentService";
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.d(TAG, "Thread id is " + Thread.currentThread().getId());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy executed");
+    }
+}
---
+++ ServiceTest/app/src/main/java/com/yxj/servicetest/MyService.java
@@ -0,0 +1,70 @@
+package com.yxj.servicetest;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import java.time.temporal.ValueRange;
+
+public class MyService extends Service {
+    private static final String TAG = "MyService";
+    private DownloadBinder mBinder = new DownloadBinder();
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate executed");
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1,notification);
+
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy executed");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+         return mBinder;
+    }
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d(TAG, "startDownload executed");
+        }
+
+        public int getProgress() {
+            Log.d(TAG, "getProgress executed");
+            return 0;
+        }
+
+    }
+
+}
